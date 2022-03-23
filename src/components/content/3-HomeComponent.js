aaa84a94cdaa3cf7490d5491d93e469e24ec5531
Patrick O'Brien <obrienp2321@gmail.com>
2022-03-23 08:16:44 +0100

    home class reconstruction

--- src/components/content/3-HomeComponent.js
+++ src/components/content/3-HomeComponent.js
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import { Col, Container, Row } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import "../styles/3-HomeComponent.css";
 
+
 function RenderHomeCards({ cardList }) {
-  return (
-    <Container fluid>
-      <NavLink to={cardList.link}>
-        <Row>
-          <Col className="carousel-inner">
-            <img src={cardList.src} alt="" className="home__img-fluid"></img>
-            <div className="carousel-caption background text">
-              <p className="carousel-caption-p">{cardList.name}</p>
-            </div>
-          </Col>
-        </Row>
-      </NavLink>
-    </Container>
-  );
+  return cardList.map((card) => {
+    return (
+      <div key={card.id} className="col-md-5 m-1">
+        <NavLink to={card.link}>
+          <Row>
+            <Col className="carousel-inner">
+              <img src={card.src} alt="" className="home__img-fluid"></img>
+              <div className="carousel-caption background text">
+                <p className="carousel-caption-p">{card.name}</p>
+              </div>
+            </Col>
+          </Row>
+        </NavLink>
+      </div>
+    );
+  });
 }
 
-const Home = (props) => {
-  let cardList;
-  if (props.home) {
-    cardList = props.home.map((card) => {
-      return (
-        <div key={card.id} className="col-md-5 m-1">
-          <RenderHomeCards cardList={card} />
-        </div>
-      );
-    });
+class Home extends  Component  {
+  constructor(props) {
+    super(props);
+    this.state={isModalOpen: false};
   }
-  return <div>{cardList}</div>;
-};
+  render() {
+    return (
+      <Container fluid>
+        <RenderHomeCards cardList={this.props.home} />
+      </Container>
+    );
+  }
+}
+
 
 export default Home;
--- src/components/content/4-InformComponent.js
+++ src/components/content/4-InformComponent.js
@@ -1,79 +1,108 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Container,
   Row,
   Col,
-  Button
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Label,
 } from "reactstrap";
+import { Control, LocalForm } from "react-redux-form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/4-InformComponent.css";
 
-function RenderInformList({ snippet }) {
-  console.log(snippet);
-  return (
-    <Row className="snippet-row">
-      <Col>
-        <img src={snippet.img} className="img-fluid" alt={snippet.alt} />
-      </Col>
-      <Col className="snippet">
-        <a href={snippet.src} target="_blank" rel="noreferrer">
-          <p className="snippet-header text-responsive">{snippet.title}</p>
-          <p className="snippet-body text-responsive">{snippet.caption}</p>
-        </a>
-      </Col>
-    </Row>
-  );
-
-  //   <Row>
-  //   <a href={snippet.src} target="_blank" rel="noreferrer">
-  //     <Col xs="3">
-  //       <CardImg alt={snippet.alt} src={snippet.img} left width="10%" />
-  //     </Col>
-  //     <Col xs="9">
-  //       <CardBody>
-  //         <CardTitle tag="h5">{snippet.title}</CardTitle>
-  //         <CardText>{snippet.caption}</CardText>
-  //       </CardBody>
-  //     </Col>
-  //   </a>
-  // </Row>
-  // );
+function RenderInformList({ informList }) {
+  return informList.map((inform) => {
+    return (
+      <div key={inform.id} className="col-md-5 m-1">
+        <Row className="snippet-row">
+          <Col>
+            <img src={inform.img} className="img-fluid" alt={inform.alt} />
+          </Col>
+          <Col className="snippet">
+            <a href={inform.src} target="_blank" rel="noreferrer">
+              <p className="snippet-header text-responsive">{inform.title}</p>
+              <p className="snippet-body text-responsive">{inform.caption}</p>
+            </a>
+          </Col>
+        </Row>
+      </div>
+    );
+  });
 }
 
-function Inform(props) {
-  let fullInform;
-  if (props.informList) {
-    fullInform = props.informList.map((inform) => {
-      return (
-        <div key={inform.id} className="col-md-5 m-1">
-          <RenderInformList snippet={inform} />
-        </div>
-      );
+class Inform extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isModalOpen: false };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
     });
   }
 
-  return (
-    <React.Fragment>
-      <Container>
-        <Row>
-          <Col sm="9" xs="5">
-            <p className="page-title">news in local waste reduction</p>
-          </Col>
-          <Col sm="3" xs="5">
-            <Button
-              className="modal-button"
-              color="success"
-              outline
-              id="suggestButton"
-            >
-              Suggest article
-            </Button>
-          </Col>
-        </Row>
-        {fullInform}
-      </Container>
-    </React.Fragment>
-  );
+  handleSubmit(values) {
+    this.toggleModal();
+    alert("suggestion submitted: " + JSON.stringify(values));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Container>
+          <Row>
+            <Col sm="9" xs="5">
+              <p className="page-title">news in local waste reduction</p>
+            </Col>
+            <Col sm="3" xs="5">
+              <Button
+                className="modal-button"
+                color="success"
+                outline
+                id="suggestButton"
+                onClick={this.toggleModal}
+              >
+                Suggest article
+              </Button>
+            </Col>
+          </Row>
+          <RenderInformList informList={this.props.informList} />
+        </Container>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader>share your thoughts</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <div className="form-group">
+                <Label htmlFor="name">name</Label>
+                <Control.text
+                  model=".name"
+                  name="name"
+                  id="name"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <Label htmlFor="comment">thought</Label>
+                <Control.text
+                  model=".comment"
+                  name="comment"
+                  id="comment"
+                  className="form-control"
+                />
+              </div>
+              <Button type="submit">submit</Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Inform;
